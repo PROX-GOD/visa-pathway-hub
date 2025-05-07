8babed89cc42366bddcc2a7cf36a681fdbef7b3d
Preshak Bhattarai <117366207+PROX-GOD@users.noreply.github.com>
2025-05-07 12:29:14 +0545

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -39,7 +39,7 @@ const Header = () => {
             <img 
               src="https://i.imgur.com/9bH2SAJ.png" 
               alt="Spring/Fall USA Logo" 
-              className="h-10 mr-2"
+              className="h-16 mr-2"
             />
           </Link>
 

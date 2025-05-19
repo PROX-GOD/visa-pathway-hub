646b7754d34ece42992ff55358367f4a2ffe8e52
Preshak Bhattarai <117366207+PROX-GOD@users.noreply.github.com>
2025-05-19 21:01:24 +0545

    Update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -335,9 +335,9 @@ const AboutPage = () => {
                 we'd love to have you as part of our growing community.
               </p>
               <div className="flex flex-wrap justify-center gap-4">
-                <Link to="/register">
+                <a href="https://t.me/SpringfallUSA" target="_blank" rel="noopener noreferrer">
                   <Button className="bg-visa-blue hover:bg-visa-navy text-white dark:bg-blue-700 dark:hover:bg-blue-800">
-                    Create an Account
+                    Join Our Telegram Group
                   </Button>
                 </Link>
                 <Link to="/visa-experiences">
